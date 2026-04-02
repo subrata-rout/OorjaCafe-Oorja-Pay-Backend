5f148528d02dfc435871ef56bac7e756c28df942
Subrata Rout <subrataa916@gmail.com>
2026-04-02 12:57:12 +0530

    feat: add AuthController for login functionality

---
+++ oorjapay/src/main/java/com/oorjacafe/oorjapay/controller/AuthController.java
@@ -0,0 +1,34 @@
+package com.oorjacafe.oorjapay.controller;
+
+
+import com.oorjacafe.oorjapay.dto.LoginDTO;
+import com.oorjacafe.oorjapay.dto.UserResponseDTO;
+import com.oorjacafe.oorjapay.entity.User;
+import com.oorjacafe.oorjapay.response.ApiResponse;
+import com.oorjacafe.oorjapay.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    //Login
+
+    public ApiResponse<UserResponseDTO> login(@RequestBody @Valid LoginDTO loginDTO){
+        User user=userService.login(loginDTO);
+        return new ApiResponse<>(
+                true,
+                "Login successful",
+                userService.mapToResponse(user)
+        );
+    }
+}
