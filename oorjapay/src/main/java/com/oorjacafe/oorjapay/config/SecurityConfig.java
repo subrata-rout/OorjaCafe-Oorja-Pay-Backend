bae3bba797dfae3bbbf947eb0bb8e133865af517
Subrata Rout <subrataa916@gmail.com>
2026-04-02 12:24:26 +0530

    feat: add spring security dependency for password encryption

--- oorjapay/src/main/java/com/oorjacafe/oorjapay/config/SecurityConfig.java
+++ oorjapay/src/main/java/com/oorjacafe/oorjapay/config/SecurityConfig.java
@@ -2,7 +2,9 @@ package com.oorjacafe.oorjapay.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -10,6 +12,17 @@ public class SecurityConfig {
     @Bean
     public BCryptPasswordEncoder passwordEncoder(){
      return new BCryptPasswordEncoder();
+    }
+
+    //Disable default security
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)throws Exception{
+        http
+                .csrf(csrf->csrf.disable())
+                .authorizeHttpRequests(auth-> auth.anyRequest()
+                        .permitAll()
+                );
+        return http.build();
     }
 
 }
